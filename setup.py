57cf692f4d2dc0df786c452b218d62abb7c37656
Farah Turkistani <ft8xx@virginia.edu>
2024-07-02 12:39:38 -0400

    added init and updated setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'BookLoverPackage',
+    version = '0.1.0',
+    author = 'Farah Turkistani',
+    author_email = 'ft8xx@virginia.edu',
+    packages = ['booklover-package'],
+    url = 'https://github.com/fturkistani/booklover-hw',
+    license = 'MIT',
+    description = 'A package that lists books that a person reads'
+)
